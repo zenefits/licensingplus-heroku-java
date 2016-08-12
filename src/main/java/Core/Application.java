4859fb182698b19912ad9045cec16aea5f654f11
scyzenefits <shuchun.yang@zenefits.com>
2016-08-11 18:35:48 -0700

    test code

--- src/main/java/Core/Application.java
+++ src/main/java/Core/Application.java
@@ -13,14 +13,14 @@ public class Application {
     public static void main(String[] args) {
 
         // Read ENV Variables
-        Configuration.LoadParams();
+        //Configuration.LoadParams();
 
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
 
         System.out.println("Launching the reconciler thread");
 
-        Reconciler lReconciler = new Reconciler();
-        lReconciler.start();
+        /*Reconciler lReconciler = new Reconciler();
+        lReconciler.start();*/
     }
 
 }
